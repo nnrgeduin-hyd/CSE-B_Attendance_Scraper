0ff9527ddfe6747ca03cd6350adaaf71ce3ea160
nnrgeduin-hyd <nnrg.edu.in@gmail.com>
2025-08-03 00:04:24 +0530

    Update CN.py

--- CN.py
+++ CN.py
@@ -64,7 +64,7 @@ def scrape_attendance(rollP):
     for attempt in range(1, MAX_ATTEMPTS + 1):
         try:
             driver = webdriver.Chrome(options=chrome_options)
-            driver.set_page_load_timeout(10)
+            driver.set_page_load_timeout(30)
             wait = WebDriverWait(driver, 5)
 
             driver.get("https://exams-nnrg.in/BeeSERP/Login.aspx")
