4b7c3354e543e8d2c563ac047df65de9b03444d2
nnrgeduin-hyd <nnrg.edu.in@gmail.com>
2025-08-03 11:26:45 +0530

    Update DEVOPS_LAB_extended.py

--- DEVOPS_LAB_extended.py
+++ DEVOPS_LAB_extended.py
@@ -10,7 +10,7 @@ import gspread
 from zoneinfo import ZoneInfo
 
 # === CONFIG ===
-SUBJECT = "CEVOPS LAB"
+SUBJECT = "DEVOPS LAB"
 SHEET_ID = "168dU0XLrRkVZQquAStktg_X9pMi3Vx9o9fOmbUYOUvA"
 CREDENTIAL_FILE = "credentials2.json"
 MAX_ATTEMPTS = 3
