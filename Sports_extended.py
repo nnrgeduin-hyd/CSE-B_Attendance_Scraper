525943064f35fc0f43cdd2da6e9a7840edca6613
nnrgeduin-hyd <nnrg.edu.in@gmail.com>
2025-08-05 18:39:16 +0530

    Update Sports_extended.py

--- Sports_extended.py
+++ Sports_extended.py
@@ -12,7 +12,7 @@ from zoneinfo import ZoneInfo
 # === CONFIG ===
 SUBJECT = "SPORTS"
 SHEET_ID = "168dU0XLrRkVZQquAStktg_X9pMi3Vx9o9fOmbUYOUvA"
-CREDENTIAL_FILE = "credentials2.json"
+CREDENTIAL_FILE = "credentials11.json"
 MAX_ATTEMPTS = 3
 BASE_PREFIX = "237Z1A05"
 THREADS = 15
