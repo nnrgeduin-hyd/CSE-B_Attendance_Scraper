37d929648aaf43756b511dd18cc740bdf2d5bed5
nnrgeduin-hyd <nnrg.edu.in@gmail.com>
2025-08-07 11:41:14 +0530

    Update scraper13.2.py

--- scraper13.2.py
+++ scraper13.2.py
@@ -28,8 +28,8 @@ SUBJECT_SHEETS = [
 SUBJECT_ALIASES = {
     "CN": "CN", "DEVOPS": "DEVOPS", "PPL": "PPL", "NLP": "NLP", "DAA": "DAA",
     "CN LAB": "CN LAB", "DEVOPS LAB": "DEVOPS LAB", "ACS LAB": "ACS LAB", "IPR": "IPR",
-    "SPORTS": "SPORTS", "MEN": "MENTORING", "ASSOC": "Association", "ASSOCIATION": "Association",
-    "LIB": "Library", "LIBRARY": "Library"
+    "SPORTS": "SPORTS", "MEN": "MENTORING", "ASSOC": "ASSOCIATION", "ASSOCIATION": "ASSOCIATION",
+    "LIB": "LIBRARY", "LIBRARY": "LIBRARY"
 }
 
 SUBJECT_ClassesAttended_RANGES = {
